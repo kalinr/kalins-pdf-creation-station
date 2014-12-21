82c72a8c3b400f915f02f56108d1916d5ae87105
Kalin Ringkvist <kalin@kalinflash.com>
2014-12-20 20:28:36 -0800

    got "grunt release" working properly. It copies all the relevant files from this git repo over into my svn repository, ready to be committed for the next release. Still needs to add some verification like version #'s and checking for missing and new files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,5 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     
@@ -9,7 +8,6 @@ module.exports = function(grunt) {
         separator: '',
       },
       dist: {
-        //src: ['vendor/angular.js', 'vendor/ui-bootstrap-tpls-0.11.2.js', 'vendor/angular-ui-sortable.js', 'vendor/ng-table.js'],
         src: ['bower_components/angular/angular.js', 'bower_components/angular-ui-sortable/sortable.js',  'bower_components/ng-table/ng-table.js', 'bower_components/angular-bootstrap/ui-bootstrap-tpls.js'],
         dest: 'dev/vendor.js'
       },
@@ -33,22 +31,55 @@ module.exports = function(grunt) {
     //phplint runs but does nothing. Can't figure out 
     phplint: {
       main: ['kalins-pdf-creation-station.php', 'kalins_pdf_admin_page.php', 'kalins_pdf_tool_page.php', 'kalins_pdf_create.php']
-    }
-    
+    }    
   });
+  
+  //function used for the 'release' task
+  var releaseFunc = function (){
+    var i = 0,
+      svnPath = "E:/stuff/Flash/WordPress Dev/kalins-pdf-creation-station/", //the root of your WordPress SVN repositiory
+      svnTrunk = svnPath + 'trunk/', //the trunk folder in our svn repository
+      aFolders = ['help', 'tcpdf'], //list of folders we need copied
+      aFiles = ['kalins_pdf_admin_page.php', 'kalins_pdf_create.php', 'kalins_pdf_styles.css', 'kalins_pdf_tool_page.php', 'kalins-pdf-creation-station.php', 
+               'KalinsUIService.js', 'readme.txt', 'vendor.min.js']; //list of files we need copied
+    
+    //TODO: make a list of unused folders and unused files, then loop through all used and unused files and folders
+    //and throw an error if there are any new or missing files or folders
+    
+    //TODO: make sure that we have added version # update info to both places in the readme.txt and updated it in 
+    //kalins-pdf-creation-station.php, bower.json and package.json
+    
+    //loop into all our folders that we need moving
+    for(i; i<aFolders.length; i++){
+      grunt.file.recurse(aFolders[i], function(abspath, rootdir, subdir, filename){
+        grunt.file.copy(abspath, svnTrunk + abspath);
+      });
+    }
 
+    //copy over all our main files
+    for(i = 0; i<aFiles.length; i++){
+      grunt.file.copy(aFiles[i], svnTrunk + aFiles[i]);
+    }
+
+    //Finally, copy over the wordpress.org assets to the root of the svn repository, one level up from the trunk directory
+    grunt.file.recurse('assets', function(abspath, rootdir, subdir, filename){
+      grunt.file.copy(abspath, svnPath + abspath);
+    });
+  }
+  
+  //load all our external modules (from node_modules dir) 
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-
   grunt.loadNpmTasks('grunt-jslint');
   grunt.loadNpmTasks('grunt-phplint');
 
+  //This is not intended to be run from command line
   grunt.registerTask('cleanup', 'delete our temp vendor file', function() {
-	grunt.file.delete('dev/vendor.js');
+    grunt.file.delete('dev/vendor.js');
   });
-
+  
+  //These are the tasks intended to run from command line
   grunt.registerTask('vendor', 'vendor tasks -- compile and minify the vendor js', ['concat', 'uglify', 'cleanup']);
-
   grunt.registerTask('lint', 'validate our JS and PHP', ['jslint', 'phplint:main']);
-
+  grunt.registerTask('release', 'move all relevant files into the proper location in the svn repository that you entered into the package.json file', releaseFunc);
 };
